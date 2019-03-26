81521f64cfc9ab483195f38a9aff97406f7ef96c
gechetspr <serhii.chepela@spryker.com>
2019-03-26 18:14:24 +0200

    TE-963 added webprofiler form plugin and fixed issues in the code

--- src/Spryker/Shared/Validator/Plugin/Form/ValidatorFormPlugin.php
+++ src/Spryker/Shared/Validator/Plugin/Form/ValidatorFormPlugin.php
@@ -5,7 +5,7 @@
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
-namespace Spryker\Shared\Validator\Plugin\FormExtension;
+namespace Spryker\Shared\Validator\Plugin\Form;
 
 use Spryker\Service\Container\ContainerInterface;
 use Spryker\Shared\FormExtension\Dependency\Plugin\FormPluginInterface;
